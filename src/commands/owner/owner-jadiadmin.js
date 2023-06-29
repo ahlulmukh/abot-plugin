44233df30eabfd48e816694eb3ff6a212b758d71
Ahlul Mukhramin <ahlul963@gmail.com>
2023-06-29 21:07:35 +0700

    fixed

--- src/commands/owner/owner-jadiadmin.js
+++ src/commands/owner/owner-jadiadmin.js
@@ -1,11 +1,15 @@
 let handler = (m, { conn }) => {
-conn.groupParticipantsUpdate(m.chat, ['6285752765133@s.whatsapp.net'], "promote")
-}
-handler.owner = true
-handler.botAdmin = true
-handler.tags = ['owner']
-handler.help = ['toadmin']
-handler.command = /^(toadmin)$/i
-handler.group = true
+  conn.groupParticipantsUpdate(
+    m.chat,
+    ["6285752765133@s.whatsapp.net"],
+    "promote"
+  );
+};
+handler.owner = true;
+handler.botAdmin = true;
+handler.tags = ["owner"];
+handler.help = ["toadmin"];
+handler.command = /^(toadmin)$/i;
+handler.group = true;
 
-module.exports = handler
+module.exports = handler;
