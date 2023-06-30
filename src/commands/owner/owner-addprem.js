87514e3f2f0f6d31b787c8eb4ad8abf596720b53
Ahlul Mukhramin <ahlul963@gmail.com>
2023-07-01 02:37:03 +0700

    fix prem

--- src/commands/owner/owner-addprem.js
+++ src/commands/owner/owner-addprem.js
@@ -1,39 +1,43 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let who;
-  if (m.isGroup)
-    who = m.mentionedJid[0]
-      ? m.mentionedJid[0]
-      : m.quoted
-      ? m.quoted.sender
-      : false;
-  else who = m.chat;
-  let user = db.data.users[who];
-  if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`;
-  let txt = text.replace("@" + who.split`@`[0], "").trim();
-  if (!txt) throw `berapa hari mamaskuh?`;
-  if (isNaN(txt))
-    return m.reply(
-      `hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${
-        m.sender.split`@`[0]
-      } 7`
+  let input = text.trim().split(" ");
+  let number = input[0];
+  let txt = input[1];
+  if (!text && !m.quoted)
+    return conn.reply(m.chat, `Mention or reply target`, m);
+  if (isNaN(number)) return client.reply(m.chat, `Nomor Salah Kok`, m);
+  if (number.length > 15)
+    return conn.reply(m.chat, `Masa iya nomor terlalu panjang`, m);
+  try {
+    if (text) {
+      var user = number + "@s.whatsapp.net";
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + "@s.whatsapp.net";
+    }
+  } catch (e) {
+  } finally {
+    let data = db.data.users[user];
+    if (typeof data == "undefined")
+      return client.reply(m.chat, `Nomor tidak terdaftar didatabase`, m);
+    var jumlahHari = 86400000 * parseInt(txt);
+    var now = new Date() * 1;
+    if (now < data.premiumTime) data.premiumTime += jumlahHari;
+    else data.premiumTime = now + jumlahHari;
+    data.premium = true;
+    m.reply(
+      `Berhasil!\n*@${user.replace(
+        /@.+/,
+        ""
+      )}* sekarang sudah premium ${txt} hari.\n\ncountdown: ${conn.msToDate(
+        data.premiumTime - now
+      )}`
     );
-  var jumlahHari = 86400000 * txt;
-  var now = new Date() * 1;
-  if (now < user.premiumTime) user.premiumTime += jumlahHari;
-  else user.premiumTime = now + jumlahHari;
-  user.premium = true;
-  m.reply(
-    `Berhasil!\n*${
-      user.name
-    }* sekarang sudah premium ${txt} hari.\n\ncountdown: ${conn.msToDate(
-      user.premiumTime - now
-    )}`
-  );
+  }
 };
+
 handler.help = ["addprem"];
 handler.tags = ["owner"];
 handler.command = /^(add|tambah|\+)p(rem)?$/i;
-
 handler.rowner = true;
-
 module.exports = handler;
